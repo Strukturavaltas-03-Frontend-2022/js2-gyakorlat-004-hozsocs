34fdd66ea399fadb63f5b31378d41be0ff31d76f
hozsocs <hozsocs@gmail.com>
2022-08-18 15:46:54 +0200

    a teszt nem megy de szerintem a program működik

--- solutions/countOfWorkingDays.js
+++ solutions/countOfWorkingDays.js
@@ -1,1 +1,44 @@
+const firstDate= new Date(2021, 0, 1);
+const secondDate= new Date(2021, 0, 1);
+let acc=0;
+const countOfWorkingDays = (firstDate, secondDate) => {
+if (firstDate.getTime() > secondDate.getTime()) { 
+    
+    throw new Error('The first parameter is earlier date, than second!')} else {
+    const days= (secondDate.getTime()-firstDate.getTime())/1000/60/60/24+1;
+    
+    const actualday = firstDate.getDate();
+    let controldate= new Date();
+    for (let index = 0; index < days; index++) {
+        controldate= new Date(firstDate.getFullYear(), firstDate.getMonth(), actualday+index);
+       // console.log(controldate);
+        if (controldate.getDay()===(0) || controldate.getDay()===6) {
+            acc=acc+0;
+            
+            }
+        else {
+            acc=acc+1
+        };
+     }
+
+     console.log(acc);       
+    }
+    return acc;
+
+};
+
+countOfWorkingDays(firstDate, secondDate);
+
+
+
+
+
+
+
+
+
+
+
+
+
 export default countOfWorkingDays;
--- solutions/getSecondsOfThisYearUntilNow.js
+++ solutions/getSecondsOfThisYearUntilNow.js
@@ -1,3 +1,9 @@
+const getSecondsOfThisYearUntilNow = () => {
+    const actualDate = new Date(2021, 10, 12, 2, 23, 23);
+    const startDate= new Date(2021, 0, 1);
+    
+   console.log(Math.round((actualDate.getTime()-startDate.getTime())/1000));
+ };
 
 export default getSecondsOfThisYearUntilNow;
 
