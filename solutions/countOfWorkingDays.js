577c0e107d32c2c405e95bee19e9716cda8b91e4
hozsocs <hozsocs@gmail.com>
2022-08-21 10:05:28 +0200

    feladat teljesítve

--- solutions/countOfWorkingDays.js
+++ solutions/countOfWorkingDays.js
@@ -1,26 +1,28 @@
 // const firstDate = new Date();
 // const secondDate = new Date();
 let acc = 0
+let days = 0
 const countOfWorkingDays = (firstDate, secondDate) => {
   if (firstDate.getTime() > secondDate.getTime()) {
     throw new Error('The first parameter is earlier date, than second!')
   } else {
-    const days = (secondDate.getTime() - firstDate.getTime()) / 1000 / 60 / 60 / 24 + 1
-
+    days = (secondDate.getTime() - firstDate.getTime()) / 1000 / 60 / 60 / 24 + 1
     const actualday = firstDate.getDate()
     let controldate = new Date()
+
     for (let index = 0; index < days; index++) {
       controldate = new Date(firstDate.getFullYear(), firstDate.getMonth(), actualday + index)
       // console.log(controldate);
-      if (controldate.getDay() === (0) || controldate.getDay() === 6) {
+      if (days === 1) acc = 1; else if
+      (controldate.getDay() === (0) || controldate.getDay() === 6) {
         acc = acc + 0
       } else {
         acc = acc + 1
       };
     }
-
-    console.log(acc)
   }
+  console.log(acc)
+
   return acc
 }
 
